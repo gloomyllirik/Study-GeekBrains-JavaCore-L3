f322feaad5401abc81e6347a7f7aa5bb7eb49e53
gloomyllirik <gloomyllirik@gmail.com>
2019-07-22 12:10:14 +0300

    дз к уроку №4

---
+++ Lesson04/src/Lesson04.java
@@ -0,0 +1,20 @@
+public class Lesson04 {
+    private final Object mon = new Object();
+    private volatile char currentLetter = 'A';
+
+    public static void main(String[] args) {
+        WaitNotifyClass w = new WaitNotifyClass();
+        Thread t1 = new Thread(() -> {
+            w.printA();
+        });
+        Thread t2 = new Thread(() -> {
+            w.printB();
+        });
+        Thread t3 = new Thread(() -> {
+            w.printC();
+        });
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
